3d8a5c05571668bc9c32bb669ccfbfacab864b3e
AlessandroDodi <dodialessandro3@gmail.com>
2025-08-04 12:09:16 +0200

    Fix build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import Hero from "@/components/home/Hero";
-import FeaturesHome from "@/components/home/FeaturesHome";
 import FAQHome from "@/components/home/FAQHome";
 import MainLayout from "@/components/layout/MainLayout";
 import TextRevealSection from "@/components/home/TextRevealSection";
